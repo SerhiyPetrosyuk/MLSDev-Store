c7ffedb043724a371b15ca40172071c104372cdd
Serhii Petrosyuk <petrosyuk@mlsdev.com>
2017-10-10 17:51:30 +0300

    Added bottom navigation view

---
+++ app/src/main/java/com/mlsdev/mlsdevstore/presentaion/custom/BottomNavigationBehavior.java
@@ -0,0 +1,59 @@
+package com.mlsdev.mlsdevstore.presentaion.custom;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v4.view.ViewCompat;
+import android.support.v4.widget.NestedScrollView;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class BottomNavigationBehavior extends CoordinatorLayout.Behavior<BottomNavigationView> {
+
+    public BottomNavigationBehavior() {
+    }
+
+    public BottomNavigationBehavior(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean layoutDependsOn(CoordinatorLayout parent, BottomNavigationView child, View dependency) {
+        return dependency instanceof NestedScrollView;
+    }
+
+    @Override
+    public boolean onStartNestedScroll(@NonNull CoordinatorLayout coordinatorLayout,
+                                       @NonNull BottomNavigationView child,
+                                       @NonNull View directTargetChild,
+                                       @NonNull View target,
+                                       int axes,
+                                       int type) {
+        return axes == ViewCompat.SCROLL_AXIS_VERTICAL;
+    }
+
+    @Override
+    public void onNestedScroll(@NonNull CoordinatorLayout coordinatorLayout,
+                               @NonNull BottomNavigationView child,
+                               @NonNull View target,
+                               int dxConsumed,
+                               int dyConsumed,
+                               int dxUnconsumed,
+                               int dyUnconsumed,
+                               int type) {
+        if (dyConsumed < 0) {
+            showBottomNavigationView(child);
+        } else if (dyConsumed > 0) {
+            hideBottomNavigationView(child);
+        }
+    }
+
+    private void hideBottomNavigationView(BottomNavigationView view) {
+        view.animate().translationY(view.getHeight());
+    }
+
+    private void showBottomNavigationView(BottomNavigationView view) {
+        view.animate().translationY(0);
+    }
+}

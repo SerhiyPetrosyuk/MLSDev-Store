dd4091a8335f5adf1cf045cba677afab51ca2135
Serhii Petrosyuk <petrosyuk@mlsdev.com>
2017-11-16 13:54:42 +0200

    Added a shared preferences manager

--- app/src/main/java/com/mlsdev/mlsdevstore/dependency_injection/module/ApplicationModule.java
+++ app/src/main/java/com/mlsdev/mlsdevstore/dependency_injection/module/ApplicationModule.java
@@ -1,7 +1,10 @@
 package com.mlsdev.mlsdevstore.dependency_injection.module;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.mlsdev.mlsdevstore.MLSDevStoreApplication;
 import com.mlsdev.mlsdevstore.dependency_injection.component.scope.ActivityScope;
 import com.mlsdev.mlsdevstore.presentaion.bottom_navigation.MainActivity;
@@ -20,6 +23,18 @@ abstract public class ApplicationModule {
     @Singleton
     static Context provideContext(MLSDevStoreApplication application) {
         return application.getApplicationContext();
+    }
+
+    @Provides
+    @Singleton
+    static SharedPreferences provideSharedPreferences(Context context) {
+        return context.getSharedPreferences("preferences", Context.MODE_PRIVATE);
+    }
+
+    @Provides
+    @Singleton
+    static Gson provideGson() {
+        return new GsonBuilder().create();
     }
 
     @ContributesAndroidInjector(modules = {ActivityModule.class})

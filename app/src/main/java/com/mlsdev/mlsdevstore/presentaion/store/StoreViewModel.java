dd5031e38b122f6273491b749ec619642d648fbe
Serhii Petrosyuk <petrosyuk@mlsdev.com>
2017-11-23 13:02:06 +0200

    Fixed styles

--- app/src/main/java/com/mlsdev/mlsdevstore/presentaion/store/StoreViewModel.java
+++ app/src/main/java/com/mlsdev/mlsdevstore/presentaion/store/StoreViewModel.java
@@ -1,11 +1,8 @@
 package com.mlsdev.mlsdevstore.presentaion.store;
 
 import android.databinding.ObservableField;
-import android.util.Log;
 
-import com.mlsdev.mlsdevstore.data.model.category.CategoryTree;
 import com.mlsdev.mlsdevstore.data.model.category.CategoryTreeNode;
-import com.mlsdev.mlsdevstore.data.remote.BaseObserver;
 import com.mlsdev.mlsdevstore.data.remote.RemoteDataSource;
 import com.mlsdev.mlsdevstore.presentaion.viewmodel.BaseViewModel;
 
@@ -24,15 +21,15 @@ public class StoreViewModel extends BaseViewModel {
 
     public void getCategories() {
 
-        remoteDataSource.getRootCategoryTree()
-                .subscribe(new BaseObserver<CategoryTree>(this) {
-                    @Override
-                    public void onSuccess(CategoryTree defaultCategoryTree) {
-                        super.onSuccess(defaultCategoryTree);
-                        Log.d(BaseViewModel.LOG_TAG, "Default category tree id: " + defaultCategoryTree.getCategoryTreeId());
-                        categories.set(defaultCategoryTree.getCategoryTreeNode().getChildCategoryTreeNodes());
-                    }
-                });
+//        remoteDataSource.getRootCategoryTree()
+//                .subscribe(new BaseObserver<CategoryTree>(this) {
+//                    @Override
+//                    public void onSuccess(CategoryTree defaultCategoryTree) {
+//                        super.onSuccess(defaultCategoryTree);
+//                        Log.d(BaseViewModel.LOG_TAG, "Default category tree id: " + defaultCategoryTree.getCategoryTreeId());
+//                        categories.set(defaultCategoryTree.getCategoryTreeNode().getChildCategoryTreeNodes());
+//                    }
+//                });
 
 
     }

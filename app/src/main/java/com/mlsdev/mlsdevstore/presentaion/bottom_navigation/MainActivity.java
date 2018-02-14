c53707b87e346873587f20de9e4ec942fbf2397b
Serhii Petrosyuk <petrosyuk@mlsdev.com>
2018-02-14 17:52:41 +0200

    Updated layouts

--- app/src/main/java/com/mlsdev/mlsdevstore/presentaion/bottom_navigation/MainActivity.java
+++ app/src/main/java/com/mlsdev/mlsdevstore/presentaion/bottom_navigation/MainActivity.java
@@ -1,21 +1,31 @@
 package com.mlsdev.mlsdevstore.presentaion.bottom_navigation;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 
 import com.mlsdev.mlsdevstore.R;
+import com.mlsdev.mlsdevstore.databinding.ActivityMainBinding;
 import com.mlsdev.mlsdevstore.presentaion.store.StoreFragment;
 
 import dagger.android.support.DaggerAppCompatActivity;
 
 public class MainActivity extends DaggerAppCompatActivity {
 
+    private ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        initBottomNavigation();
 
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.content, new StoreFragment())
                 .commit();
+    }
+
+    private void initBottomNavigation() {
+
     }
 }

4a2e440d69321b91d73122114fefb88683c51ab4
Serhii Petrosyuk <petrosyuk@mlsdev.com>
2018-02-26 17:52:34 +0200

    Added the `checkout` button

--- app/src/main/java/com/mlsdev/mlsdevstore/presentaion/cart/CartViewModel.java
+++ app/src/main/java/com/mlsdev/mlsdevstore/presentaion/cart/CartViewModel.java
@@ -3,6 +3,8 @@ package com.mlsdev.mlsdevstore.presentaion.cart;
 import android.arch.lifecycle.Lifecycle;
 import android.arch.lifecycle.OnLifecycleEvent;
 import android.databinding.ObservableBoolean;
+import android.view.View;
+import android.widget.Toast;
 
 import com.mlsdev.mlsdevstore.data.cart.Cart;
 import com.mlsdev.mlsdevstore.presentaion.viewmodel.BaseViewModel;
@@ -31,5 +33,10 @@ public class CartViewModel extends BaseViewModel implements Cart.OnItemCountChan
     @Override
     public void onItemCountChanged(int count) {
         cartIsEmpty.set(count == 0);
+    }
+
+    public void onCheckoutClick(View button) {
+        // TODO: 2/26/18 start the checkout screen
+        Toast.makeText(button.getContext(), "it will start the checkout screen", Toast.LENGTH_SHORT).show();
     }
 }

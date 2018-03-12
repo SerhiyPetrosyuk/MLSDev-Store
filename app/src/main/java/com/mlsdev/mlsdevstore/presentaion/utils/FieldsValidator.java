269e3dde9f57ff70ad113912e9d430c3a516fb17
Serhii Petrosyuk <petrosyuk@mlsdev.com>
2018-03-12 17:26:26 +0200

    Refactored the `Fields validator`

--- app/src/main/java/com/mlsdev/mlsdevstore/presentaion/utils/FieldsValidator.java
+++ app/src/main/java/com/mlsdev/mlsdevstore/presentaion/utils/FieldsValidator.java
@@ -1,7 +1,6 @@
 package com.mlsdev.mlsdevstore.presentaion.utils;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.util.ArrayMap;
 import android.text.TextUtils;
@@ -15,7 +14,6 @@ import java.util.Map;
 import javax.inject.Inject;
 
 import io.reactivex.Completable;
-import io.reactivex.Single;
 
 import static com.mlsdev.mlsdevstore.presentaion.utils.FieldsValidator.Field.EMAIL;
 
@@ -35,27 +33,6 @@ public class FieldsValidator {
         fieldsForValidation.put(key, value);
         return this;
     }
-
-    public Single<Map<String, String>> validate() {
-        return validate(fieldsForValidation);
-    }
-
-    public Single<Map<String, String>> validate(@NonNull Map<String, String> fields) {
-        return Single.fromCallable(() -> {
-            invalidFields.clear();
-
-            for (Map.Entry<String, String> entry : fields.entrySet())
-                if (TextUtils.isEmpty(entry.getValue()))
-                    invalidFields.put(entry.getKey(), context.getString(R.string.error_empty_field));
-
-            if (!invalidFields.containsKey(EMAIL) && fields.containsKey(EMAIL) && !validateEmail(fields.get(EMAIL)))
-                invalidFields.put(EMAIL, context.getString(R.string.error_incorrect_field));
-
-            fieldsForValidation.clear();
-            return invalidFields;
-        });
-    }
-
 
     public Completable validateFields() {
         return Completable.create(emitter -> {
@@ -77,10 +54,6 @@ public class FieldsValidator {
         });
     }
 
-    public boolean isValid() {
-        return invalidFields.isEmpty();
-    }
-
     public Map<String, String> getInvalidFields() {
         return invalidFields;
     }
@@ -91,8 +64,6 @@ public class FieldsValidator {
 
     public @interface Field {
         String EMAIL = "email";
-        String PASSWORD = "password";
-        String CONFIRM_PASSWORD = "confirm_password";
         String FIRST_NAME = "first_name";
         String LAST_NAME = "last_name";
     }

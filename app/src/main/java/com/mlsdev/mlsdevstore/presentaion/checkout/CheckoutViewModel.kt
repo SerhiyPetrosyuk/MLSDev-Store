2fa7c71edc52edbb876737adcac7affce77d4556
Serhiy Petrosyuk <petrosyuk@mlsdev.com>
2019-01-24 18:01:43 +0200

    Added custom edit texts

--- app/src/main/java/com/mlsdev/mlsdevstore/presentaion/checkout/CheckoutViewModel.kt
+++ app/src/main/java/com/mlsdev/mlsdevstore/presentaion/checkout/CheckoutViewModel.kt
@@ -1,8 +1,15 @@
 package com.mlsdev.mlsdevstore.presentaion.checkout
 
+import androidx.databinding.ObservableField
+import com.mlsdev.mlsdevstore.MLSDevStoreApplication
 import com.mlsdev.mlsdevstore.presentaion.viewmodel.BaseViewModel
-
 import javax.inject.Inject
 
 class CheckoutViewModel @Inject
-constructor() : BaseViewModel()
+constructor(val application: MLSDevStoreApplication) : BaseViewModel() {
+
+    val cardHolder = ObservableField<String>()
+    val cardNumber = ObservableField<String>()
+    val cardExpirationDate = ObservableField<String>()
+
+}

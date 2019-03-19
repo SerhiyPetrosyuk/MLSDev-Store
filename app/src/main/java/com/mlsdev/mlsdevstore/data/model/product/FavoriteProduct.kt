0e6124b57800d0d4f933ddbcccf54d1145fb1e84
SerhiyPetrosuk <petrosyuk@mlsdev.com>
2019-03-19 17:47:08 +0200

    added new entity

---
+++ app/src/main/java/com/mlsdev/mlsdevstore/data/model/product/FavoriteProduct.kt
@@ -0,0 +1,19 @@
+package com.mlsdev.mlsdevstore.data.model.product
+
+import androidx.room.ColumnInfo
+import androidx.room.Embedded
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.mlsdev.mlsdevstore.data.local.database.tables.FavoritesTable
+
+@Entity(tableName = FavoritesTable.NAME)
+data class FavoriteProduct(
+        @Embedded
+        val product: Product) {
+
+    @PrimaryKey
+    @ColumnInfo(name = FavoritesTable.COLUMN_ID)
+    val id = product.itemId
+
+    fun isFavorite(): Boolean = product.isFavorite
+}

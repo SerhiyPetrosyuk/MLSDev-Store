1c4e3627df66551b437801cc5d26018096c78c95
Serhii Petrosyuk <petrosyuk@mlsdev.com>
2019-03-27 00:12:06 +0200

    adde a deeplink

--- app/src/main/java/com/mlsdev/mlsdevstore/backgroudworks/RemindWorker.kt
+++ app/src/main/java/com/mlsdev/mlsdevstore/backgroudworks/RemindWorker.kt
@@ -6,12 +6,16 @@ import android.content.Context
 import android.os.Build
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import androidx.navigation.NavDeepLinkBuilder
 import androidx.room.Room
 import androidx.work.RxWorker
 import androidx.work.WorkerParameters
 import com.mlsdev.mlsdevstore.R
 import com.mlsdev.mlsdevstore.data.local.database.AppDatabase
+import com.mlsdev.mlsdevstore.data.model.category.CategoryTreeNode
+import com.mlsdev.mlsdevstore.presentaion.products.ProductsFragmentArgs
 import io.reactivex.Single
+import kotlin.random.Random
 
 class RemindWorker(context: Context, workerParams: WorkerParameters) : RxWorker(context, workerParams) {
 
@@ -22,19 +26,33 @@ class RemindWorker(context: Context, workerParams: WorkerParameters) : RxWorker(
     private val channelId = "reminder_channel_id"
 
     override fun createWork(): Single<Result> {
-        return Single.fromCallable { sendNotification() }
+        return database.categoriesDao().queryCategoryTreeNode()
+                .map { it[Random.nextInt(0, it.size)] }
+                .doOnSuccess { sendNotification(it) }
                 .map { Result.success() }
                 .onErrorReturn { Result.failure() }
     }
 
-    private fun sendNotification() {
+    private fun sendNotification(category: CategoryTreeNode) {
         createNotificationChannel()
         val notificationId = 2019
+
+        val argsBuilder = ProductsFragmentArgs.Builder()
+        argsBuilder.categoryId = category.category.categoryId
+        argsBuilder.categoryName = category.category.categoryName
+
+        val pendingIntent = NavDeepLinkBuilder(applicationContext)
+                .setGraph(R.navigation.app_nav)
+                .setDestination(R.id.products_fragment)
+                .setArguments(argsBuilder.build().toBundle())
+                .createPendingIntent()
+
         val notificationBuilder = NotificationCompat.Builder(applicationContext, channelId)
                 .setSmallIcon(R.drawable.ic_mlsdev_logo)
-                .setContentTitle("Test title")
-                .setContentText("Test notification text")
+                .setContentTitle("MLSDev Store")
+                .setContentText("Check new products in the ${category.category.categoryName}")
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setContentIntent(pendingIntent)
                 .setAutoCancel(true)
 
         with(NotificationManagerCompat.from(applicationContext)) {
